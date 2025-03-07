c2c5d27bc283f949dd79367e427210d332569b42
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-07 23:09:59 +0600

    add new asset constants for user images, doodles, quotes, and stars

--- lib/app/core/constants/_assets.dart
+++ lib/app/core/constants/_assets.dart
@@ -1,8 +1,20 @@
-class AppAssets{
+class AppAssets {
   // logo
   static const String logo = 'assets/logo/logo_org.svg';
   static const String logoSquareWithBackground = 'assets/logo/1.svg';
   static const String logoFullWithBackground = 'assets/logo/2.svg';
 
-  
+  // person
+  static const String person = 'assets/images/hero-user_2.svg';
+
+  // doodles
+  static const String doodle1 = 'assets/svgs/doodle_1.svg';
+  static const String doodle2 = 'assets/svgs/doodle_2.svg';
+  static const String doodle3 = 'assets/svgs/doodle_3.svg';
+
+  // quotes
+  static const String quote = 'assets/svgs/quote.svg';
+
+  // stars
+  static const String star = 'assets/svgs/star_2.svg';
 }
