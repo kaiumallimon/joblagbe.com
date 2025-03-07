dd3b971153f763f3f639b17cfed0d5830eb979fa
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-07 23:09:55 +0600

    add user testimonials to app strings for enhanced engagement

--- lib/app/core/constants/_strings.dart
+++ lib/app/core/constants/_strings.dart
@@ -5,4 +5,14 @@ class AppStrings {
       'Unlock Your Potential\nwith New Job Opportunities';
   static const String heroDetails =
       'Our platform features more than 1.5 million job vacancies, connecting you with employers who value your skills and experiences.';
+
+  static const String quote1 =
+      "I just landed on a designer job! I used the\nsite constantly during my job search.";
+  static const String quote1Author = "John Doe";
+  static const String quote2Title = "\"Best for job seekers, I like it\"";
+
+  static const String quote2 =
+      "It allows you to search for jobs by keywords and location.\nHowever, some job listings my redirect you to external sites, complicating the application process.";
+  static const String quote2Author = "Mary Jane";
+  static const String quote2AuthorPosition = "Project Manager, BMW";
 }
