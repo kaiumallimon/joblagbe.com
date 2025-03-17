db1f1ad9b93167991b65ffc8c5e1a402ef9c3052
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-18 01:13:12 +0600

    add ApplicantWrapperController and layout for dashboard navigation

---
+++ lib/app/modules/dashboard/applicant/wrapper/controller/_applicant_wrapper_controller.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+
+class ApplicantWrapperController extends GetxController {
+  RxList<Map<String, dynamic>> sideMenus = [
+    {
+      "title": "Home",
+      "icon": Icons.home,
+      "route": "/dashboard/applicant/home",
+    },
+    {
+      "title": "Jobs",
+      "icon": Icons.work,
+      "route": "/dashboard/applicant/jobs",
+    },
+    // Add more menu items here if needed
+  ].obs; // Make it observable if it needs to change dynamically
+
+  var selectedMenuIndex = 0.obs;
+  var hoveredMenuIndex = (-1).obs;
+
+  void onHover(int index) {
+    hoveredMenuIndex.value = index;
+  }
+
+  void syncMenuWithRoute(BuildContext context) {
+    String currentRoute = GoRouter.of(context).state.path!;
+
+    for (int i = 0; i < sideMenus.length; i++) {
+      if (currentRoute == sideMenus[i]["route"]) {
+        selectedMenuIndex.value = i;
+        break;
+      }
+    }
+  }
+}
