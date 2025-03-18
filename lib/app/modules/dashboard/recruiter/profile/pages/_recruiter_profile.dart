6a7a0ff713d4b3da0442b4938a7e46f6ba3fec9f
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-18 21:02:23 +0600

    add Recruiter Profile page with basic layout

---
+++ lib/app/modules/dashboard/recruiter/profile/pages/_recruiter_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecruiterProfilePage extends StatelessWidget {
+  const RecruiterProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recruiter Profile'),
+      ),
+      body: Center(
+        child: const Text('Recruiter Profile Page'),
+      ),
+    );
+  }
+}
