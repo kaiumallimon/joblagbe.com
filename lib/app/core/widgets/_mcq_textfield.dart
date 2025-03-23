8d083f1a316fd97533961b91c059fa80c82e3f88
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-24 00:07:39 +0600

    add MCQTextfield widget: implement customizable text field with input formatting and styling

---
+++ lib/app/core/widgets/_mcq_textfield.dart
@@ -0,0 +1,46 @@
+// ignore: must_be_immutable
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../theming/colors/_colors.dart';
+
+class MCQTextfield extends StatelessWidget {
+  MCQTextfield(
+      {super.key,
+      this.controller,
+      this.hintText,
+      this.onChanged,
+      this.width,
+      this.inputFormatters});
+
+  final TextEditingController? controller;
+  final String? hintText;
+  void Function(String)? onChanged;
+  final List<TextInputFormatter>? inputFormatters;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: width ?? double.infinity,
+      child: TextField(
+        inputFormatters: inputFormatters,
+        onChanged: onChanged,
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: hintText,
+          border: InputBorder.none,
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: AppColors.primary.withOpacity(.5), width: 2)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: AppColors.black.withOpacity(.15), width: 2)),
+        ),
+      ),
+    );
+  }
+}
