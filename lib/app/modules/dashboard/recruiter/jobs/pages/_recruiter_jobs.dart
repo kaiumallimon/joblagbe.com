0018b89a7ad96898c207a064e2ff90f9288eecfb
Kaium Al Limon <limon.3cs@gmail.com>
2025-03-24 20:05:11 +0600

    add(RecruiterJobsPage): implement job fetching on page load and update app bar title

--- lib/app/modules/dashboard/recruiter/jobs/pages/_recruiter_jobs.dart
+++ lib/app/modules/dashboard/recruiter/jobs/pages/_recruiter_jobs.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:joblagbe/app/core/theming/colors/_colors.dart';
+import 'package:joblagbe/app/core/widgets/_dashboard_appbar.dart';
+import 'package:joblagbe/app/modules/dashboard/recruiter/jobs/controllers/_recruiter_jobs_controller.dart';
 
 class RecruiterJobsPage extends StatelessWidget {
   const RecruiterJobsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final jobsController = Get.put(RecruiterJobsController());
+
+    // Fetch jobs when the page is built
+    if (jobsController.jobs.value == null) {
+      jobsController.fetchJobs(context);
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recruiter Jobs'),
-      ),
+      backgroundColor: AppColors.white,
+      appBar: dashboardAppbar('Explore/Manage Jobs'),
       body: Center(
         child: Text(
           'Welcome to the Recruiter Jobs Page!',
